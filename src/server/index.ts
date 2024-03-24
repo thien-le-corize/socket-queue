9097c349426a3a00a3aa60000d8000bc41636b2d
thien-le-corize <thien.le@corize.co.jp>
2024-03-24 23:18:12 +0700

    update v1

--- src/server/index.ts
+++ src/server/index.ts
@@ -63,8 +63,8 @@ const main = async () => {
                 })
             })
         })
-        app.listen({ port: PORT }, (err, address) => {
-            console.log(`Server listening at ${address}`)
+        app.listen(PORT, '0.0.0.0', () => {
+            console.log(`Server listening on port ${PORT}`)
         })
     } catch (error) {
         if (error) {
